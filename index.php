db3df0b0ba5499d971a35d694e9f6b4195279f0a
EHOt63 <90418258+EHOt63@users.noreply.github.com>
2021-10-11 15:16:29 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+		 <link rel="stylesheet" href="master.css">
+			<title>
+				
+			</title>
+		
+		</head>
+		<body>
+			
+			<h1>
+			ghbdtn vbh
+			</h1>
+			<?php
+				echo("<p>hi</p>");
+			?>
+			
+		</body>
+	</html>
